22670b2e46cdfc0515d47a512482c59e93a75b3c
agala <agalaaustin@yahoo.com>
2023-03-18 15:56:44 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that print numbers from 0 to 10
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
