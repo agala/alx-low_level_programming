5d69305436ba4f33e6a00297da87b8f7c2744985
agala <agalaaustin@yahoo.com>
2023-03-18 15:58:49 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that print numbers from 0 to 10
+ * main - program that print numbers from 0 to 9
  *
  * Return: 0 (Success)
 */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar(i);
 	}
