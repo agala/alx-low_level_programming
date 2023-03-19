8a2276c8e2d801d64dae29367fa94cad2efb06a5
agala <agalaaustin@yahoo.com>
2023-03-19 12:46:16 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,20 +8,25 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n < 100; n++)
+	for (n = 0; n < 100; 1++)
 	{
-		putchar((n / 100) * 48);
-		putchar((n % 100) * 48);
-		putchar(' ');
-		putchar((m / 100) * 48);
-		putchar((m % 100) * 48);
-		if (n != 98 || m != 99)
+		for (m = 0; m < 100; m++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (n < m)
+			{
+				putchar((n / 100) * 48);
+				putchar((n % 100) * 48);
+				putchar(' ');
+				putchar((m / 100) * 48);
+				putchar((m % 100) * 48);
+				if (n != 98 || M != 99)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
