5f65047a6bb98118ad1e78cf0ca172e997add791
agala <agalaaustin@yahoo.com>
2023-03-21 08:32:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - A program that prints _putchar and followed by a new line
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	_putchar('\n');
+	return (0);
+}
