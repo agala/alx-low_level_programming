9074c90df9e300fe72383cec666cc55fc614e03f
agala <agalaaustin@yahoo.com>
2023-03-21 10:03:49 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main_h>
+
+/**
+ * main - progarm that prints alphabets in lowercase followed by a new line
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+
+	char alph = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar("\n", alph);
+	return (0);
+}
