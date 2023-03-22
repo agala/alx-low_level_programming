2bf90d5e0c9517b22de3a013327c06328dc4a760
agala <agalaaustin@yahoo.com>
2023-03-22 03:32:35 -0700

    10x Alpabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 -  make alphabet 10 times
  * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	int x;
 	char letter;
+	int x;
+	x = 0;
 
 	while (x < 10)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-	_putchar(letter);
-	}
-	_putchar('\n');
-	x++;
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		_putchar(letter);
+		}
+		_putchar('\n');
+		x++;
 	}
 }
