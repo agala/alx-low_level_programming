c8565ca8ad3fde7772b8236e381cc093bd74bf47
agala <agalaaustin@yahoo.com>
2023-04-03 01:58:28 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,32 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ * rot13 - Write a function that encodes a string using rot13
+ *
+ * @hi: This is my input string
+ *
+ * Return: String converted to rot13
+ *
+ */
+
+char *rot13(char *hi)
 {
-  int n;
-  int a[5];
-  int *p;
+	int index, count;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	char minus[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char mayus[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (index = 0; hi[index] != '\0'; ++index)
+	{
+		for (count = 0; minus[count] != '\0' ; count++)
+		{
+			if (hi[index] == minus[count])
+			{
+				hi[index] = mayus[count];
+				break;
+			}
+		}
+	}
+	hi[index] = '\0';
+	return (hi);
 }
